653d15167956b0cc654750bf2c631321b555aca9
CheyenneKong <cheyenne1010.ck@gmail.com>
2026-05-02 14:34:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.write("ISOM5240")
-st.write("ISOM5240")
-st.write("ISOM5240")
-st.write("ISOM5240")
 
+import streamlit as st
+
+st.write("Hello, *World!* :sunglasses:")
